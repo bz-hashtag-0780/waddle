fee62950821a750b0660c9c7a4da94b88e104cb6
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2025-05-18 01:58:59 +0400

    waddle logo

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -18,11 +18,15 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 						{/* Logo and Navigation */}
 						<div className="flex">
 							<div className="flex-shrink-0 flex items-center">
-								<Link
-									href="/"
-									className="text-2xl font-bold text-[rgb(117,206,254)]"
-								>
-									Waddle $5G
+								<Link href="/" className="flex items-center">
+									<img
+										src="/waddle_logo.png"
+										alt="Waddle Logo"
+										className="h-8 w-8 mr-2 rounded-lg"
+									/>
+									<span className="text-2xl font-bold text-[rgb(117,206,254)]">
+										Waddle $5G
+									</span>
 								</Link>
 							</div>
 							<nav className="ml-6 flex space-x-8">
@@ -31,12 +35,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 									className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
 								>
 									Dashboard
-								</Link>
-								<Link
-									href="/hotspots"
-									className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
-								>
-									Hotspots
 								</Link>
 								<Link
 									href="/network"
