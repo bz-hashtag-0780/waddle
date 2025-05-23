2258f1625b4bcfaa512d819b11ea61cdbc269a68
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2025-05-23 19:49:01 +0400

    adding opensea (post-hackathon submission)
    Just adding opensea link,
    This is a post-hackathon commit, so not official part of the hackathon submission.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -147,6 +147,14 @@ export default function Home() {
 								>
 									Features
 								</Link>
+								<Link
+									href="https://opensea.io/collection/waddle-5g-operators"
+									className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									OpenSea
+								</Link>
 							</div>
 							<div className="hidden md:absolute md:inset-y-0 md:right-0 md:flex md:items-center md:justify-end">
 								{user ? (
--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -55,6 +55,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 								>
 									Account Linking
 								</Link>
+								<Link
+									href="https://opensea.io/collection/waddle-5g-operators"
+									className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									OpenSea
+								</Link>
 							</nav>
 						</div>
 
